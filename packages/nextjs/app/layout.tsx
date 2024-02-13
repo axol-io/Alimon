54e9614f6cc48c6908b54a5b697e9465648518cb
Randee <randeelayosa@gmail.com>
2024-02-13 00:05:40 -0800

    Build Deposit and Overview pages, and add btn styles.

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -47,6 +47,41 @@ export const metadata: Metadata = {
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
   return (
     <html>
+      <head>
+        <style>
+          {`
+            @font-face {
+              font-family: 'ChakraPetchRegular';
+              src: url('../fonts/ChakraPetch-Regular.ttf') format('truetype'); /* Path to regular font file */
+              font-weight: normal;
+            }
+  
+            @font-face {
+              font-family: 'ChakraPetchMedium';
+              src: url('../fonts/ChakraPetch-Medium.ttf') format('truetype'); /* Path to regular font file */
+              font-weight: 300;
+            }
+  
+            @font-face {
+              font-family: 'ChakraPetchBold';
+              src: url('../fonts/ChakraPetch-Bold.ttf') format('truetype'); /* Path to bold font file */
+              font-weight: bold; /* Specify the font weight */
+            }
+  
+            @font-face {
+              font-family: 'ChakraPetchSemiBold';
+              src: url('../fonts/ChakraPetch-SemiBold.ttf') format('truetype'); /* Path to italic font file */
+              font-style: 500; /* Specify the font style */
+            }
+  
+            @font-face {
+              font-family: 'ChakraPetchLight';
+              src: url('../fonts/ChakraPetch-Light.ttf') format('truetype'); /* Path to italic font file */
+              font-style: 200; /* Specify the font style */
+            }
+          `}
+        </style>
+      </head>
       <body>
         <ScaffoldEthAppWithProviders>{children}</ScaffoldEthAppWithProviders>
       </body>
