0ae697edbea22587a7a1d82df3d90b93a44facd8
Randee <randeelayosa@gmail.com>
2024-02-14 04:29:35 -0800

    Restyle Deposit page. Add toggle buttons and icons.

--- packages/nextjs/app/deposit/page.tsx
+++ packages/nextjs/app/deposit/page.tsx
@@ -17,34 +17,57 @@ const Deposits: NextPage = () => {
 
           {/* Updated grid setting to always have 1 column */}
           <div className="py-8">
-            <div className="grid grid-cols-1 my-0 mx-auto max-w-2xl">
-              <div className="card bg-base-200 border border-neutral border-b-0 rounded-b-none p-4">
-                <p className="card-title">Available</p>
-                <div className="flex flex-row justify-between items-end border-neutral border-b-2 mb-4 pb-3">
-                  <p className="text-7xl">XXXX</p>
-                  <p className="text-bold">ETH</p>
+            <div className="grid grid-cols-1 my-0 mx-auto max-w-xl">
+
+
+
+            <div className="inline-flex max-w-md rounded-lg shadow-sm rounded-b-none" role="group">
+              <button type="button" className="inline-flex justify-center px-4 py-3 w-full rounded-s-2xl rounded-bl-none text-md border dark:border-neutral dark:text-primary dark:hover:text-white dark:hover:bg-neutral dark:focus:text-secondary dark:focus:bg-base-300">
+                <img src="./stake-icon.svg" className="w-8 pr-3" />
+                Stake
+              </button>
+              <button type="button" className="inline-flex justify-center px-4 py-3 w-full rounded-e-2xl rounded-br-none text-md border dark:border-neutral dark:text-primary dark:hover:text-white dark:hover:bg-neutral dark:focus:text-secondary dark:focus:bg-base-300">
+                <img src="./unstake-icon.svg" className="w-11 pr-3" />
+                Unstake
+              </button>
+            </div>
+
+
+              <div className="card flex sm:flex-col md:flex-row justify-around bg-base-200 border border-neutral border-b-0 rounded-none max-w-md px-4">
+                <div className="flex flex-col">
+                  <p className="card-title text-sm mb-0 max-w-xs mx-auto">Available</p>
+                  <div className="flex flex-row justify-between items-end border-neutral sm:border-b-2 md:border-b-0 max-w-xs mx-auto">
+                    <p className="text-4xl pr-2 leading-none">XXXX</p>
+                    <p className="text-bold text-sm">ETH</p>
+                  </div>
                 </div>
-                <p className="text-xl text-bold">Deposit</p>
-                <div className="flex flex-row justify-between items-end">
-                  <p className="text-7xl">XXXX</p>
-                  <p className="text-bold">ETH</p>
+
+                <div className="flex flex-col">
+                  <p className="text-sm text-bold mb-0 max-w-xs mx-auto">Deposit</p>
+                  <div className="flex flex-row justify-between items-end max-w-xs mx-auto">
+                    <p className="text-4xl pr-2 leading-none">XXXX</p>
+                    <p className="text-bold text-sm">ETH</p>
+                  </div>
                 </div>
               </div>
 
-              <div className="card bg-base-300 border border-neutral border-t-0 rounded-t-none px-4 py-6">
-                <div className="my-0 mx-auto text-center">
-                  <div className="flex flex-row bg-white w-full rounded-full pr-4">
-                    <input type="number" placeholder="ETH amount" className="input bg-white w-full max-w-xs" />
+              <div className="card bg-base-300 border border-neutral border-t-0 rounded-t-none max-w-md px-4 py-6">
+                <div className="my-0 mx-auto text-center w-full">
+                  <div className="flex flex-row bg-white rounded-full pr-4 mx-auto w-full max-w-sm">
+                    <input type="number" placeholder="ETH amount" className="input bg-white w-full" />
                     <button className="btn btn-sm rounded-md border-0 my-2">Max</button>
                   </div>
-                  <button className="btn btn-blast btn-wide my-4">Deposit Now</button>
+                  <button className="btn btn-blast btn-block max-w-sm my-4">Deposit Now</button>
                 </div>
                 
-                <div className="flex flex-row justify-between items-end">
-                  <p className="text-secondary">You will gain</p>
-                  <p className="text-secondary">fETH</p>
+                <div className="flex flex-row justify-between items-end mx-auto w-full max-w-sm">
+                  <p className="text-secondary text-base">You will gain</p>
+                  <p className="text-secondary text-base">cETH</p>
                 </div>
-                <p className="text-secondary">Alimon statistics</p>
+                <div className="flex flex-row justify-between items-end mx-auto w-full max-w-sm">
+                  <p className="text-secondary text-base">You will gain</p>
+                  <p className="text-secondary text-base">fETH</p>
+                </div>
               </div>
             </div>
           </div>
