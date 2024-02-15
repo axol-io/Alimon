038a3c6a366a64adbb9cfd37e32f7b9337ed7023
Randee <randeelayosa@gmail.com>
2024-02-15 00:34:52 -0800

    Clean up layouts and add graphics.

--- packages/nextjs/app/overview/page.tsx
+++ packages/nextjs/app/overview/page.tsx
@@ -7,8 +7,23 @@ const Overview: NextPage = () => {
     <div className="flex flex-row min-h-screen bg-base-100">
       <Menu />
       <div className="flex-grow">
-        <div className="px-10 pt-10">
-          <h1 className="text-3xl font-bold">Overview</h1>
+        <div className="px-10">
+
+          <svg className="w-full" fill="none" height="15" width="1177" xmlns="http://www.w3.org/2000/svg">
+            <path d="M653.476 7.969h29.202v6.489h-29.202zM.918 7.969H30.12v6.489H.918zM701.334 7.969h29.202v6.489h-29.202zM48.776 7.969h29.202v6.489H48.776zM511 7.965h138.691v6.489H511zM1077.49 7.965h64v6.489h-64zM944.612 7.969h29.202v6.489h-29.202zM690.465 7.969h3.082v6.489h-3.082zM37.907 7.969h3.082v6.489h-3.082zM751.303 7.969h3.082v6.489h-3.082zM98.745 7.969h3.082v6.489h-3.082zM756.333 7.969h3.082v6.489h-3.082zM103.774 7.969h3.082v6.489h-3.082zM868.686 7.969h3.082v6.489h-3.082zM873.715 7.965h3.082v6.489h-3.082zM1145.94 7.965h3.082v6.489h-3.082zM1159.67 7.965h3.082v6.489h-3.082zM1173.41 7.965h3.082v6.489h-3.082zM937.744 7.969h3.082v6.489h-3.082zM821.226 7.969h3.082v6.489h-3.082zM168.668 7.969h3.082v6.489h-3.082z" fill="currentColor"></path>
+            <circle cx="843.128" cy="11.374" fill="currentColor" r="1.46"></circle>
+            <circle cx="916.059" cy="11.374" fill="currentColor" r="1.46"></circle>
+            <circle cx="979.06" cy="11.374" fill="currentColor" r="1.46"></circle>
+            <circle cx="847.995" cy="11.374" fill="currentColor" r="1.46"></circle>
+            <circle cx="920.926" cy="11.374" fill="currentColor" r="1.46"></circle>
+            <circle cx="983.927" cy="11.374" fill="currentColor" r="1.46"></circle>
+            <circle cx="852.862" cy="11.37" fill="currentColor" r="1.46"></circle>
+            <circle cx="925.794" cy="11.37" fill="currentColor" r="1.46"></circle>
+            <circle cx="988.794" cy="11.37" fill="currentColor" r="1.46"></circle>
+            <path d="M1176 1 0 1.01M458.489 1l-12.5 12.5h-230.5" stroke="currentColor" stroke-width="1.5"></path>
+          </svg>
+
+          <h1 className="text-3xl font-bold pt-8">Overview</h1>
           {/* <div className="text-center mt-4"> */}
             {/* Correctly use Link component with content */}
             {/* <Link href="/"
@@ -22,7 +37,9 @@ const Overview: NextPage = () => {
                 <p className="card-title">cETH Deposited</p>
                 <p className="text-7xl">XXXX</p>
               </div>
-              <button className="btn btn-blast btn-wide my-4">Deposit Now</button>
+              <div className="w-full flex justify-center">
+                <button className="btn btn-blast btn-wide my-4">Deposit Now</button>
+              </div>
             </div>
             <div>
               <div className="card bg-base-200 border-neutral border p-4">
@@ -53,10 +70,31 @@ const Overview: NextPage = () => {
                 <p className="card-title">APY</p>
                 <p className="text-7xl">XXXX</p>
               </div>
-              <button className="btn btn-blast btn-wide my-4">Claim Yield</button>
+              <div className="w-full flex justify-center">
+                <button className="btn btn-blast btn-wide my-4">Claim Yield</button>
+              </div>
             </div>
           </div>
         </div>
+
+        <div className="container mx-auto px-8 pt-4">
+          <div className="w-full">
+            <svg className="w-full" fill="none" height="15" width="1177" xmlns="http://www.w3.org/2000/svg">
+              <path d="M653.476 7.969h29.202v6.489h-29.202zM.918 7.969H30.12v6.489H.918zM701.334 7.969h29.202v6.489h-29.202zM48.776 7.969h29.202v6.489H48.776zM511 7.965h138.691v6.489H511zM1077.49 7.965h64v6.489h-64zM944.612 7.969h29.202v6.489h-29.202zM690.465 7.969h3.082v6.489h-3.082zM37.907 7.969h3.082v6.489h-3.082zM751.303 7.969h3.082v6.489h-3.082zM98.745 7.969h3.082v6.489h-3.082zM756.333 7.969h3.082v6.489h-3.082zM103.774 7.969h3.082v6.489h-3.082zM868.686 7.969h3.082v6.489h-3.082zM873.715 7.965h3.082v6.489h-3.082zM1145.94 7.965h3.082v6.489h-3.082zM1159.67 7.965h3.082v6.489h-3.082zM1173.41 7.965h3.082v6.489h-3.082zM937.744 7.969h3.082v6.489h-3.082zM821.226 7.969h3.082v6.489h-3.082zM168.668 7.969h3.082v6.489h-3.082z" fill="currentColor"></path>
+              <circle cx="843.128" cy="11.374" fill="currentColor" r="1.46"></circle>
+              <circle cx="916.059" cy="11.374" fill="currentColor" r="1.46"></circle>
+              <circle cx="979.06" cy="11.374" fill="currentColor" r="1.46"></circle>
+              <circle cx="847.995" cy="11.374" fill="currentColor" r="1.46"></circle>
+              <circle cx="920.926" cy="11.374" fill="currentColor" r="1.46"></circle>
+              <circle cx="983.927" cy="11.374" fill="currentColor" r="1.46"></circle>
+              <circle cx="852.862" cy="11.37" fill="currentColor" r="1.46"></circle>
+              <circle cx="925.794" cy="11.37" fill="currentColor" r="1.46"></circle>
+              <circle cx="988.794" cy="11.37" fill="currentColor" r="1.46"></circle>
+              <path d="M1176 1 0 1.01M458.489 1l-12.5 12.5h-230.5" stroke="currentColor" stroke-width="1.5"></path>
+            </svg>
+          </div>
+        </div>
+
       </div>
     </div>
   );
--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -29,24 +29,59 @@ const Home: NextPage = () => {
           </li>
         </ul>
       </div>
-      <div className="flex-grow splash">
-        <div className="px-10 pt-10">
-          <h1 className="text-center mb-8">
-            <span className="block text-2xl mb-2">Alimon</span>
-          </h1>
-          <div className="grid grid-cols-12 gap my-8">
-            <div className="card col-span-2">
-              <figure className="w-full"><img src="./illustration-planet-pie-half-left.webp" className="w-28 my-0 mx-auto" /></figure>
-            </div>
-            <div className="card p-4 col-span-8 text-center">
-              <p>Alimon.io, issues fixed yield swaps for yield-generating assets without overcollateralization, capitalizing on Blast’s native yield rebase.</p>
-            </div>
-            <div className="card col-span-2">
-              <figure className="w-full"><img src="./illustration-planet-pie-half-right.webp" className="w-28 my-0 mx-auto" /></figure>
+
+      <div className="flex-grow pt-4">
+        <div className="container mx-auto px-8 pt-8 pb-6">
+          <div className="w-full">
+            <svg className="w-full" fill="none" height="15" width="1177" xmlns="http://www.w3.org/2000/svg">
+              <path d="M653.476 7.969h29.202v6.489h-29.202zM.918 7.969H30.12v6.489H.918zM701.334 7.969h29.202v6.489h-29.202zM48.776 7.969h29.202v6.489H48.776zM511 7.965h138.691v6.489H511zM1077.49 7.965h64v6.489h-64zM944.612 7.969h29.202v6.489h-29.202zM690.465 7.969h3.082v6.489h-3.082zM37.907 7.969h3.082v6.489h-3.082zM751.303 7.969h3.082v6.489h-3.082zM98.745 7.969h3.082v6.489h-3.082zM756.333 7.969h3.082v6.489h-3.082zM103.774 7.969h3.082v6.489h-3.082zM868.686 7.969h3.082v6.489h-3.082zM873.715 7.965h3.082v6.489h-3.082zM1145.94 7.965h3.082v6.489h-3.082zM1159.67 7.965h3.082v6.489h-3.082zM1173.41 7.965h3.082v6.489h-3.082zM937.744 7.969h3.082v6.489h-3.082zM821.226 7.969h3.082v6.489h-3.082zM168.668 7.969h3.082v6.489h-3.082z" fill="currentColor"></path>
+              <circle cx="843.128" cy="11.374" fill="currentColor" r="1.46"></circle>
+              <circle cx="916.059" cy="11.374" fill="currentColor" r="1.46"></circle>
+              <circle cx="979.06" cy="11.374" fill="currentColor" r="1.46"></circle>
+              <circle cx="847.995" cy="11.374" fill="currentColor" r="1.46"></circle>
+              <circle cx="920.926" cy="11.374" fill="currentColor" r="1.46"></circle>
+              <circle cx="983.927" cy="11.374" fill="currentColor" r="1.46"></circle>
+              <circle cx="852.862" cy="11.37" fill="currentColor" r="1.46"></circle>
+              <circle cx="925.794" cy="11.37" fill="currentColor" r="1.46"></circle>
+              <circle cx="988.794" cy="11.37" fill="currentColor" r="1.46"></circle>
+              <path d="M1176 1 0 1.01M458.489 1l-12.5 12.5h-230.5" stroke="currentColor" stroke-width="1.5"></path>
+            </svg>
+          </div>
+        </div>
+        <div className="splash max-w-6xl">
+          <div className="px-10 pt-10 pb-8">
+            <h1 className="text-center mb-8">
+              <span className="block mb-2">Alimon</span>
+            </h1>
+            <div className="grid grid-cols-12 gap-1 my-8">
+              <div className="card col-span-2">
+                <figure className="w-full"><img src="./illustration-planet-pie-half-left.webp" className="w-28 my-0 mx-auto" /></figure>
+              </div>
+              <div className="card p-1 col-span-8 text-center">
+                <p className="text-2xl">Alimon.io, issues fixed yield swaps for yield-generating assets without overcollateralization, capitalizing on Blast’s native yield rebase.</p>
+              </div>
+              <div className="card col-span-2">
+                <figure className="w-full"><img src="./illustration-planet-pie-half-right.webp" className="w-28 my-0 mx-auto" /></figure>
+              </div>
             </div>
           </div>
         </div>
-        <div className="container mx-auto px-4 py-8">
+        <div className="container mx-auto px-8 py-8">
+          <div className="w-full">
+            <svg className="w-full" fill="none" height="15" width="1177" xmlns="http://www.w3.org/2000/svg">
+              <path d="M653.476 7.969h29.202v6.489h-29.202zM.918 7.969H30.12v6.489H.918zM701.334 7.969h29.202v6.489h-29.202zM48.776 7.969h29.202v6.489H48.776zM511 7.965h138.691v6.489H511zM1077.49 7.965h64v6.489h-64zM944.612 7.969h29.202v6.489h-29.202zM690.465 7.969h3.082v6.489h-3.082zM37.907 7.969h3.082v6.489h-3.082zM751.303 7.969h3.082v6.489h-3.082zM98.745 7.969h3.082v6.489h-3.082zM756.333 7.969h3.082v6.489h-3.082zM103.774 7.969h3.082v6.489h-3.082zM868.686 7.969h3.082v6.489h-3.082zM873.715 7.965h3.082v6.489h-3.082zM1145.94 7.965h3.082v6.489h-3.082zM1159.67 7.965h3.082v6.489h-3.082zM1173.41 7.965h3.082v6.489h-3.082zM937.744 7.969h3.082v6.489h-3.082zM821.226 7.969h3.082v6.489h-3.082zM168.668 7.969h3.082v6.489h-3.082z" fill="currentColor"></path>
+              <circle cx="843.128" cy="11.374" fill="currentColor" r="1.46"></circle>
+              <circle cx="916.059" cy="11.374" fill="currentColor" r="1.46"></circle>
+              <circle cx="979.06" cy="11.374" fill="currentColor" r="1.46"></circle>
+              <circle cx="847.995" cy="11.374" fill="currentColor" r="1.46"></circle>
+              <circle cx="920.926" cy="11.374" fill="currentColor" r="1.46"></circle>
+              <circle cx="983.927" cy="11.374" fill="currentColor" r="1.46"></circle>
+              <circle cx="852.862" cy="11.37" fill="currentColor" r="1.46"></circle>
+              <circle cx="925.794" cy="11.37" fill="currentColor" r="1.46"></circle>
+              <circle cx="988.794" cy="11.37" fill="currentColor" r="1.46"></circle>
+              <path d="M1176 1 0 1.01M458.489 1l-12.5 12.5h-230.5" stroke="currentColor" stroke-width="1.5"></path>
+            </svg>
+          </div>
           <footer className="text-center mt-8"></footer>
         </div>
       </div>
